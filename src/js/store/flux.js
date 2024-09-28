5b3f34e2e1199bc41754f5aabd79c50efe5f28bf
Marian <marianvsf@gmail.com>
2024-09-28 05:22:16 +0000

    first commit

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,44 +1,46 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
+			contacts: [
 			]
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
+	
+			getContactsList: async ()=>{
+        let resp = await fetch("https://playground.4geeks.com/contact/agendas/marian", {
+          method: "GET",
+          headers: {
+            "Content-Type":"aplication/json",
+          }
+        });
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
+        if (resp.status === 404) {
+          await fetch("https://playground.4geeks.com/contact/agendas/marian",{        
+            method: "POST", 
+            headers: {
+              "Content-Type":"aplication/json",
+            }
+        });
+        }
+      
+        if (resp.ok) {
+          console.log("Agenda creada")
+          let dataContacts = await resp.json();
+          console.log( {dataContacts} )
+          setStore( {contacts: dataContacts } )
+        }
+  
+      },
+      createNewContact:()=>{
 
-				//reset the global store
-				setStore({ demo: demo });
-			}
-		}
+      },
+      updateContact:()=>{
+
+      },
+      deleteContact:()=>{
+        
+      }
+    }
 	};
 };
 
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -8,6 +8,7 @@ export const Home = () => (
 		<p>
 			<img src={rigoImage} />
 		</p>
+		
 		<a href="#" className="btn btn-success">
 			If you see this green button, bootstrap is working
 		</a>
