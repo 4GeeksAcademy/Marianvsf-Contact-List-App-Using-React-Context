5788f5d75f43f15ee5b97ce943e2ffc8e3612855
Marian <marianvsf@gmail.com>
2024-10-06 01:23:05 +0000

    footer

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -3,8 +3,8 @@ import React, { Component } from "react";
 export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center fixed-bottom">
 		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
+			Made by<a href="https://github.com/Marianvsf"> Marian Suárez </a> on 
+			<a href="http://www.4geeksacademy.com"> 4Geeks Academy</a>
 		</p>
 	</footer>
 );
